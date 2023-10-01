7610b1380f2100322e72d5b3b5a6ac3bb2854663
hiroro-work <hiroro@sonicgarden.jp>
2023-10-01 10:38:11 +0900

    認証データを取得するカスタムフック作成

---
+++ src/utils/firebase/auth/auth.ts
@@ -0,0 +1,5 @@
+import { getAuth, getIdTokenResult } from 'firebase/auth';
+import type { User, ParsedToken } from 'firebase/auth';
+
+export type { User, ParsedToken };
+export { getAuth, getIdTokenResult };
