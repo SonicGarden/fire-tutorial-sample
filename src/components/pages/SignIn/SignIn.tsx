4b2a89abdf4f120ea78e7561fc824d6d238d7a28
hiroro-work <hiroro@sonicgarden.jp>
2023-10-01 11:53:54 +0900

    サインインできるように

--- src/components/pages/SignIn/SignIn.tsx
+++ src/components/pages/SignIn/SignIn.tsx
@@ -1,3 +1,10 @@
+import { Stack } from '@mantine/core';
+import { SignInWithEmailAndPasswordForm } from '@/components/forms/auth/SignInWithEmailAndPasswordForm';
+
 export const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <Stack maw={500} mx='auto' py='lg'>
+      <SignInWithEmailAndPasswordForm />
+    </Stack>
+  );
 };
--- src/utils/firebase/auth/auth.ts
+++ src/utils/firebase/auth/auth.ts
@@ -2,6 +2,7 @@ import {
   createUserWithEmailAndPassword as _createUserWithEmailAndPassword,
   getAuth,
   getIdTokenResult,
+  signInWithEmailAndPassword as _signInWithEmailAndPassword,
   signOut as _signOut,
 } from 'firebase/auth';
 import type { User, ParsedToken } from 'firebase/auth';
@@ -9,7 +10,10 @@ import type { User, ParsedToken } from 'firebase/auth';
 const createUserWithEmailAndPassword = async (email: string, password: string) =>
   _createUserWithEmailAndPassword(getAuth(), email, password);
 
+const signInWithEmailAndPassword = async (email: string, password: string) =>
+  _signInWithEmailAndPassword(getAuth(), email, password);
+
 const signOut = async () => _signOut(getAuth());
 
 export type { User, ParsedToken };
-export { createUserWithEmailAndPassword, getAuth, getIdTokenResult, signOut };
+export { createUserWithEmailAndPassword, getAuth, getIdTokenResult, signInWithEmailAndPassword, signOut };
