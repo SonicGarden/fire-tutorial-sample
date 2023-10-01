002a80955b530a0c15ea987fc7ea03f04c76381b
hiroro-work <hiroro@sonicgarden.jp>
2023-10-01 11:37:12 +0900

    タイトルをセミボールドに

--- src/layouts/Layout/Layout.tsx
+++ src/layouts/Layout/Layout.tsx
@@ -1,10 +1,11 @@
+import { Text } from '@mantine/core';
 import { ResponsiveLayout, useResponsiveLayoutContext } from '@/components/layouts/ResponsiveLayout';
 import type { ReactNode } from 'react';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <ResponsiveLayout
-      header={{ title: 'Firebaseチュートリアル' }}
+      header={{ title: <Text fw={500}>Firebaseチュートリアル</Text> }}
       navbar={{ navMenu: 'ナビメニュー', accountMenu: 'アカウントメニュー' }}
     >
       {children}
