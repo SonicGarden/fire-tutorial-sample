6594ce8f2db13a30c0faf035200d0bf09e415a7e
hiroro-work <hiroro@sonicgarden.jp>
2023-10-03 08:43:29 +0900

    404エラー表示するときはデフォルトのレイアウト適用するように

--- src/layouts/Layout/Layout.tsx
+++ src/layouts/Layout/Layout.tsx
@@ -4,12 +4,14 @@ import { AccountMenu } from '../_components/AccountMenu';
 import { Title } from '../_components/Title';
 import type { ReactNode } from 'react';
 
-export const Layout = withAuth(({ children }: { children: ReactNode }) => {
+export const _Layout = ({ children }: { children: ReactNode }) => {
   return (
     <ResponsiveLayout header={{ title: <Title /> }} navbar={{ navMenu: 'ナビメニュー', accountMenu: <AccountMenu /> }}>
       {children}
     </ResponsiveLayout>
   );
-});
+};
+
+export const Layout = withAuth(_Layout);
 
 export const useLayout = useResponsiveLayoutContext;
