67c850e6192b7e61670acfc53e87b87d7ee6000f
hiroro-work <hiroro@sonicgarden.jp>
2023-10-12 13:22:06 +0900

    ルートページをApp Routerに

--- src/components/pages/Root/Root.tsx
+++ src/components/pages/Root/Root.tsx
@@ -1,8 +1,9 @@
-import { useRouter } from 'next/router';
+'use client';
+import { useRouter } from 'next/navigation';
 import { LoadingScreen } from '@/components/screens/LoadingScreen';
 
 export const Root = () => {
   const router = useRouter();
-  router.isReady && router.replace('/books');
+  router.replace('/books');
   return <LoadingScreen />;
 };
