fec48799f9ae8618870a633970deb265a97808df
hiroro-work <hiroro@sonicgarden.jp>
2023-10-12 16:41:32 +0900

    warningが出ないように

--- src/components/pages/Root/Root.tsx
+++ src/components/pages/Root/Root.tsx
@@ -1,9 +1,15 @@
 'use client';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import { LoadingScreen } from '@/components/screens/LoadingScreen';
 
 export const Root = () => {
   const router = useRouter();
-  router.replace('/books');
+
+  useEffect(() => {
+    router.replace('/books');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return <LoadingScreen />;
 };
--- src/components/pages/admin/AdminRoot/AdminRoot.tsx
+++ src/components/pages/admin/AdminRoot/AdminRoot.tsx
@@ -1,9 +1,15 @@
 'use client';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import { LoadingScreen } from '@/components/screens/LoadingScreen';
 
 export const AdminRoot = () => {
   const router = useRouter();
-  router.replace('/admin/books');
+
+  useEffect(() => {
+    router.replace('/admin/books');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return <LoadingScreen />;
 };
